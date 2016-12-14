27c858a0db25164860d227be8dedef6d26072c83
Mariano <mariano.palliser@gmail.com>
2016-12-14 23:24:16 +0100

    Delete Ejercicio4.py

--- Ejercicio4.py
+++
@@ -1,6 +0,0 @@
-altura = int (input("Introduce los metros de altura de la finca"))
-anchura = int (input("Introduce los metros de anchura"))
-area = altura * anchura
-perimetro = 2 * (altura + anchura)
-print ("El area de la finca es" , area)
-print ("El perimetro de la finca es" , perimetro)
