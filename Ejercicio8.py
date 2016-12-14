e0aac20d554e285b7214d3869c0b8cc773db2fe8
Mariano <mariano.palliser@gmail.com>
2016-12-14 23:23:49 +0100

    Delete Ejercicio8.py

--- Ejercicio8.py
+++
@@ -1,8 +0,0 @@
-primer_numero = int (input("Introduce el primer numero"))
-segundo_numero = int (input("Introduce el segundo numero"))
-if primer_numero > segundo_numero:
-    print ("El primer numero" , primer_numero ,"Es mayor que", segundo_numero)
-elif primer_numero == segundo_numero:
-    print ("Los numeros son iguales", primer_numero)
-else:
-    print ("El primer numero", primer_numero , "Es menor que el segundo numero", segundo_numero)
